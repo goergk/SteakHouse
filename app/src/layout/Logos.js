e138787647489e11c79380adec4aa8757be1c270
goergk <grzegorzkaw124@gmail.com>
2022-07-19 22:10:46 +0200

    Updated infinite scroll in Logos section

--- app/src/layout/Logos.js
+++ app/src/layout/Logos.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Logo } from '../components/logos/index';
 import { Logos_data } from '../data/data';
+import { useWindowSize } from '../helpers/useWindowSize';
 import '../scss/style.scss';
 
 const Logos = () => {
+
+  const size = useWindowSize();
+
   return (
     <div className='Logos'>
       <div className='Logos__container'>
         {Logos_data.map((logo, index) => {
           return <Logo key={index} src={logo.src} alt={logo.alt} />;
         })}
+        {size < 751 &&
+          Logos_data.map((logo, index) => {
+            return <Logo key={index} src={logo.src} alt={logo.alt} />;
+          })
+        }
       </div>
     </div>
   )
