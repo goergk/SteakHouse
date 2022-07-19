ba24eb457f06f92452f5bfcfc5aa78d07fe8181a
goergk <grzegorzkaw124@gmail.com>
2022-07-19 20:07:34 +0200

    Fixed styling

--- app/src/layout/Logos.js
+++ app/src/layout/Logos.js
@@ -4,15 +4,15 @@ import { Logos_data } from '../data/data';
 import '../scss/style.scss';
 
 const Logos = () => {
-	return (
-		<div className='Logos'>
-			<div className='Logos__container'>
-				{Logos_data.map((logo, index) => {
-					return <Logo key={index} src={logo.src} alt={logo.alt} />;
-				})}
-			</div>
-		</div>
-	)
+  return (
+    <div className='Logos'>
+      <div className='Logos__container'>
+        {Logos_data.map((logo, index) => {
+          return <Logo key={index} src={logo.src} alt={logo.alt} />;
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Logos;
